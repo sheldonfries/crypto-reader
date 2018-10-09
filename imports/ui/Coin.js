8a83654176425348be4b4cd918cd8f8ef4de71fe
sheldonfries <sheldonfries@gmail.com>
2018-10-08 17:03:44 -0700

    Added removal of coins from portfolio

--- imports/ui/Coin.js
+++ imports/ui/Coin.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import { Coins } from '../api/coins.js';
+
 export default class Coin extends Component {
-  render() {
+	deleteCoin() {
+		// TODO: Add prompt to remove, etc. and refresh top bar after.
+		Coins.remove(this.props.coin._id);
+	}
+	
+	render() {
     return (
     	<tr>
-    		<td><button className="delete btn">&times;</button></td>
+    		<td><button className="delete btn" onClick={this.deleteCoin.bind(this)}>&times;</button></td>
 		    <td>{this.props.coin.num}</td>
 		    <td>{this.props.coin.text}</td>
 		    <td>${this.props.coin.price}</td>
